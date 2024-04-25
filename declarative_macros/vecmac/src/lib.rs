964c754ff8b86c7daa153e1b7f450b5499e3f218
Leo Zarni <leokhantzarni@gmail.com>
2024-04-26 02:47:34 +1000

    added more macros

--- declarative_macros/vecmac/src/lib.rs
+++ declarative_macros/vecmac/src/lib.rs
@@ -3,10 +3,23 @@ macro_rules! avec {
     () => {
         Vec::new()
     };
-    ($element: expr) => { 
+
+    ($($element: expr),+ $(,)? ) => { 
         {
             let mut vs = Vec::new();
-            vs.push($element);
+            $(vs.push($element);)* // repeat vs.push(element) as many times as indicated by the
+                                   // asterisk
+            vs
+        }
+    };
+
+    ($element: expr; $count: expr) => {
+        {
+            let mut vs = Vec::new();
+            let x = $element;
+            for _ in 0..$count {
+                vs.push(x.clone());
+            }
             vs
         }
     };
@@ -24,4 +37,23 @@ fn single() {
     assert!(!x.is_empty());
     assert_eq!(x.len(), 1);
     assert_eq!(x[0], 42);
+}
+
+#[test]
+fn double() {
+    let x: Vec<u32> = avec!(42, 43);
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 2);
+    assert_eq!(x[0], 42);
+    assert_eq!(x[1], 43);
+}
+
+#[test]
+fn clone_2_nonliteral() {
+    let mut y = Some(42);
+    let x: Vec<u32> = avec!(y.take().unwrap(); 2); // push 42 2 times
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 2);
+    assert_eq!(x[0], 42);
+    assert_eq!(x[1], 42);
 }
